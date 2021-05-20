f5fbffa7766c192ef20415e2c37274428e9e87c3
Susan Ashmore <scashmorecitcom@gmail.com>
2021-05-19 23:48:47 -0400

    text color settings

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import styleNav from '../pages/Settings'
 
 import Logo from './Logo'
 import Links from './Links'
--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -2,25 +2,48 @@ import React, { Component } from 'react'
 
 class Settings extends Component {
     state = {
-        color: '#212529'
+        color: '#212529',
+        text: '#fff',
     }
     navChangeHandler = e => {
         this.setState({
             color: e.target.value
         })
     }
+    textChangeHandler = e => {
+        this.setState({
+            text: e.target.value
+        })
+    }
     render () {
         const styleNav = {
-            background: this.state.color
+            background: this.state.color,
+            color: this.state.text,
         }
         return(
-            <div style={styleNav} className='thing'>
+            <>
+            <div className='thing'>
+                <div className='bgcolor'>
+                <label>Nav Color:</label> {' '}
                 <input 
                 onChange={this.navChangeHandler}
                 value={this.setState.color}
                 type="color"
                 />
+                </div>
+                <div className='txtcolor'>
+                <label>Text Color:</label> {' '}
+                <input 
+                onChange={this.textChangeHandler}
+                value={this.setState.text}
+                type="color"
+                />
+                </div>
             </div>
+            <div style={styleNav}>
+                <h1>Hello World</h1>
+            </div>
+            </>
         )
     }
 }
