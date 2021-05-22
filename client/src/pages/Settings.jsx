050c2768f0e9fdadb6930d4cf5ef0d5bd85018e9
Susan Ashmore <scashmorecitcom@gmail.com>
2021-05-21 22:08:12 -0400

    added settings in modal

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,69 +1,102 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 class Settings extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showHide: false
+        }
+    }
+
+    handleModalShowHide() {
+        this.setState({ showHide: !this.state.showHide })
+    }
+
     state = {
         color: '#212529',
         text: '#fff',
         fontFamily: 'arial',
-    }
+    };
     navChangeHandler = e => {
         this.setState({
             color: e.target.value
         })
-    }
+    };
     textChangeHandler = e => {
         this.setState({
             text: e.target.value
         })
-    }
+    };
     fontChangeHandler = e => {
         this.setState({
             font: e.target.value
         })
-    }
-    render () {
+    };
+
+    render() {
         const styleNav = {
             background: this.state.color,
             color: this.state.text,
             fontFamily: this.state.font,
         }
-        return(
+        return (
             <>
-            <div className='thing'>
-                <div className='bgcolor'>
-                <label>Nav Color:</label> {' '}
-                <input 
-                onChange={this.navChangeHandler}
-                value={this.setState.color}
-                type="color"
-                />
+                <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+                    Page Settings
+                </Button>
+
+                <Modal show={this.state.showHide}>
+                    <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
+                        <Modal.Title>Page Settings</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className='thing'>
+                            <div className='bgcolor'>
+                                <label>Nav Color:</label> {' '}
+                                <input
+                                    onChange={this.navChangeHandler}
+                                    value={this.setState.color}
+                                    type="color"
+                                />
+                            </div>
+                            <div className='txtcolor'>
+                                <label>Text Color:</label> {' '}
+                                <input
+                                    onChange={this.textChangeHandler}
+                                    value={this.setState.text}
+                                    type="color"
+                                />
+                            </div>
+                            <div className='font'>
+                                <label>Font:</label> {' '}
+                                <select
+                                    onChange={this.fontChangeHandler}
+                                    value={this.setState.font}>
+                                    <option value='arial'>Arial</option>
+                                    <option value='cambria'>Cambria</option>
+                                    <option value='comic sans ms'>Comic Sans</option>
+                                    <option value='courier new'>Courier New</option>
+                                    <option value='georgia'>Georgia</option>
+                                    <option value='times'>Times</option>
+                                    <option value='verdana'>Verdana</option>
+                                </select>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => this.handleModalShowHide()}>
+                            Close
+                    </Button>
+                        <Button variant="primary" onClick={() => this.handleModalShowHide()}>
+                            Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+                <div style={styleNav}>
+                    <h1>Hello World</h1>
                 </div>
-                <div className='txtcolor'>
-                <label>Text Color:</label> {' '}
-                <input 
-                onChange={this.textChangeHandler}
-                value={this.setState.text}
-                type="color"
-                />
-                </div>
-                <div className='font'>
-                <label>Font:</label> {' '}
-                <select
-                onChange={this.fontChangeHandler}
-                value={this.setState.font}>
-                    <option value='arial'>Arial</option>
-                    <option value='cambria'>Cambria</option>
-                    <option value='comic sans ms'>Comic Sans</option>
-                    <option value='courier new'>Courier New</option>
-                    <option value='georgia'>Georgia</option>
-                    <option value='times'>Times</option>
-                    <option value='verdana'>Verdana</option>
-                </select>
-                </div>
-            </div>
-            <div style={styleNav}>
-                <h1>Hello World</h1>
-            </div>
             </>
         )
     }
